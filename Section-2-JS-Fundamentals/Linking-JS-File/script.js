d1c089ef8f207aa4a3055398baa72c829f1eca71
Edveika <edvinasbureika@gmail.com>
2024-02-27 23:05:27 +0200

    Variable types, switched to the single qoutes

--- Section-2-JS-Fundamentals/Linking-JS-File/script.js
+++ Section-2-JS-Fundamentals/Linking-JS-File/script.js
@@ -1,5 +1,17 @@
-let js = "amazing";
-if (js == "amazing") alert("javascript is amazing");
-else alert("javascript is not amazing");
+let js = 'amazing';
+if (js == 'amazing') alert('javascript is amazing');
+else alert('javascript is not amazing');
 
 console.log(2 + 2);
+
+// Variable holds boolean value
+let jsIsFun = false;
+
+// Type is going to be a boolean
+console.log(typeof jsIsFun);
+
+// Changes variable value and type to string
+jsIsFun = 'actually, it is quite fun!';
+
+// Type is now going to be a string
+console.log(typeof jsIsFun);
