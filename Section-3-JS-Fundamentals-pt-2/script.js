40a0a53b44f88b2c8d9dbcfc14de4658a438e484
Edveika <edvinasbureika@gmail.com>
2024-03-03 23:16:18 +0200

    Functions

--- Section-3-JS-Fundamentals-pt-2/script.js
+++ Section-3-JS-Fundamentals-pt-2/script.js
@@ -1,9 +1,16 @@
+// Enabled strict mode
 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-// If strict mode is enabled, error will be shown in console as hasDriverLicense is not a valid variable name
-// if (passTest) hasDriverLicense = true;
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('Can drive');
+// // If strict mode is enabled, error will be shown in console as hasDriverLicense is not a valid variable name
+// // if (passTest) hasDriverLicense = true;
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('Can drive');
+
+function logger(text) {
+  console.log(text);
+}
+
+logger('My name is Edvinas');
