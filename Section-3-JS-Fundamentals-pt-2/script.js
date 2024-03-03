b6974d4a043fe059c6ce2d03eba527aea044f8f9
Edveika <edvinasbureika@gmail.com>
2024-03-03 23:36:36 +0200

    Arrow functions

--- Section-3-JS-Fundamentals-pt-2/script.js
+++ Section-3-JS-Fundamentals-pt-2/script.js
@@ -9,6 +9,7 @@
 // if (passTest) hasDriversLicense = true;
 // if (hasDriversLicense) console.log('Can drive');
 
+// Function definition
 function logger(text) {
   console.log(text);
 }
@@ -22,3 +23,17 @@ const calcAge = function (birthYear) {
 
 const curAge = calcAge(1991);
 console.log(curAge);
+
+// Arrow function(birth year is a parameter)
+const calcAge1 = (birthYear) => 2077 - birthYear;
+const curAge1 = calcAge1(1991);
+console.log(curAge1);
+
+const yearsUntilRetire = (birthYear) => {
+  const age = 2077 - birthYear;
+  const retirement = 65 - age;
+  return retirement;
+};
+
+const retirement = yearsUntilRetire(1991);
+console.log(retirement);
