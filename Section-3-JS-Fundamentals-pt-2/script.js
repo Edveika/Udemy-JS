4a754c895e6f6eb4cd09b043d41606afd64502df
Edveika <edvinasbureika@gmail.com>
2024-03-04 21:11:55 +0200

    Challange #4

--- Section-3-JS-Fundamentals-pt-2/script.js
+++ Section-3-JS-Fundamentals-pt-2/script.js
@@ -261,8 +261,39 @@
 //   ++rep;
 // }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
-while (dice !== 6) {
-  dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//   dice = Math.trunc(Math.random() * 6) + 1;
+//   console.log(dice);
+// }
+
+//
+// CHallange #4
+//
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+/* Write your code below. Good luck! 🙂 */
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+for (let i = 0; i < bills.length; ++i) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = tips[i] + bills[i];
 }
+
+function calcAverage(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
