69090f3ed62e4ef5ae772bb939cae08a0e46eca1
Edveika <edvinasbureika@gmail.com>
2024-03-09 15:01:33 +0200

    DOM manipulation

--- Section-7-DOM-and-Events/script.js
+++ Section-7-DOM-and-Events/script.js
@@ -1,3 +1,10 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct number!';
+document.querySelector('.number').textContent = 7331;
+document.querySelector('.score').textContent = 12;
+document.querySelector('.highscore').textContent = 1337;
+
+document.querySelector('.guess');
