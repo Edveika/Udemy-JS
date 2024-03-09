7eed8e97da145eab618021a6fa4416584206af6a
Edveika <edvinasbureika@gmail.com>
2024-03-09 23:15:41 +0200

    Refactor

--- Section-7-DOM-and-Events/Modal-Window/script.js
+++ Section-7-DOM-and-Events/Modal-Window/script.js
@@ -5,21 +5,28 @@ const btnHide = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
 for (let i = 0; i < btnShow.length; ++i) {
   btnShow[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+    openModal();
   });
 }
 
 btnHide.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  closeModal();
 });
 
 overlay.addEventListener('click', function () {
   if (overlay.classList.contains('hidden')) return;
 
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  closeModal();
 });
