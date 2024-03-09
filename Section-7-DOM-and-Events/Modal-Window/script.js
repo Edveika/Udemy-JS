3876367bf07a8d3c1a9d260ca40ffd30675d938a
Edveika <edvinasbureika@gmail.com>
2024-03-09 23:07:31 +0200

    Modal open/close logic

---
+++ Section-7-DOM-and-Events/Modal-Window/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const btnShow = document.querySelectorAll('.show-modal');
+const btnHide = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < btnShow.length; ++i) {
+  btnShow[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnHide.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
