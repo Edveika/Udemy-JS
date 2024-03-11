327e560e7ef8366222b8718682de81b2cf1bdd34
Edveika <edvinasbureika@gmail.com>
2024-03-11 16:06:00 +0200

    Pig game objects stored in variables

---
+++ Section-7-DOM-and-Events/Pig-Game/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Player 0 and 1 HTML objects
+const p0Obj = document.querySelector('.player--0');
+const p1Obj = document.querySelector('.player--1');
+const p0ScoreObj = document.querySelector('#score--0');
+const p1ScoreObj = document.querySelector('#score--1');
+const p0CurScoreObj = document.querySelector('#current--0');
+const p1CurScoreObj = document.querySelector('#current--1');
+
+// GUI HTML objects
+const diceImgObj = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
