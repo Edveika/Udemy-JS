17c931a54b37c4fb4d64868b2b79974df13797a8
Edveika <edvinasbureika@gmail.com>
2024-03-11 16:32:07 +0200

    Pig game rolling the dice

--- Section-7-DOM-and-Events/Pig-Game/script.js
+++ Section-7-DOM-and-Events/Pig-Game/script.js
@@ -13,3 +13,59 @@ const diceImgObj = document.querySelector('.dice');
 const btnNewGame = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+let curRoller = 0;
+let diceNum = getRandomInt(6);
+p0ScoreObj.textContent = '0';
+p1ScoreObj.textContent = '0';
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function updateDiceImg(num) {
+  if (num > 6 || num < 1) return;
+
+  diceImgObj.src = `img/dice-${num}.png`;
+}
+
+function rollDice() {
+  diceNum = getRandomInt(6);
+  updateDiceImg(diceNum);
+}
+
+function switchPlayer() {
+  if (curRoller === 0) {
+    curRoller = 1;
+    p0Obj.classList.remove('player--active');
+    p1Obj.classList.add('player--active');
+  } else {
+    curRoller = 0;
+    p1Obj.classList.remove('player--active');
+    p0Obj.classList.add('player--active');
+  }
+}
+
+btnRoll.addEventListener('click', function () {
+  rollDice();
+
+  if (curRoller === 0) {
+    if (diceNum === 1) {
+      switchPlayer();
+      p0CurScoreObj.textContent = 0;
+    } else {
+      p0CurScoreObj.textContent = diceNum;
+    }
+  } else {
+    if (diceNum === 1) {
+      switchPlayer();
+      p1CurScoreObj.textContent = 0;
+    } else {
+      p1CurScoreObj.textContent = diceNum;
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {});
+
+btnNewGame.addEventListener('click', function () {});
