b95e0b80d47c46c5004072f8b58e9b398b1a52a5
Edveika <edvinasbureika@gmail.com>
2024-05-07 23:31:50 +0300

    rest param

--- Section-9-DS-Operators-Strings/script.js
+++ Section-9-DS-Operators-Strings/script.js
@@ -508,3 +508,9 @@ const [mainKeyword, ...rest] = books[0].keywords;
 const { publisher: bookPublisher, ...restOfTheBook } = books[1];
 console.log(bookPublisher);
 console.log(restOfTheBook);
+
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount("Algorithms", "Robert Sedgewick", "Kevin Wayne");
