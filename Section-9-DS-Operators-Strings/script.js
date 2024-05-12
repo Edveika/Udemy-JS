7643c5221561df934f2c8c4e858398b47acbe893
Edveika <edvinasbureika@gmail.com>
2024-05-12 21:15:58 +0300

    Looping over obj keys

--- Section-9-DS-Operators-Strings/script.js
+++ Section-9-DS-Operators-Strings/script.js
@@ -58,31 +58,31 @@ const restaurant = {
 // Object looping
 //
 
-// Property names
-const properties = Object.keys(openingHours);
-let openStr = `We are open on ${properties.length} days: `;
+// // Property names
+// const properties = Object.keys(openingHours);
+// let openStr = `We are open on ${properties.length} days: `;
 
-for (const day of properties) {
-  console.log(day);
-  openStr += `${day}, `;
-}
+// for (const day of properties) {
+//   console.log(day);
+//   openStr += `${day}, `;
+// }
 
-console.log(openStr);
+// console.log(openStr);
 
-// Property values
-const values = Object.values(openingHours);
+// // Property values
+// const values = Object.values(openingHours);
 
-for (const value of values) {
-  console.log(value);
-}
+// for (const value of values) {
+//   console.log(value);
+// }
 
-// Entire obj
-const entries = Object.entries(openingHours);
-console.log(entries);
+// // Entire obj
+// const entries = Object.entries(openingHours);
+// console.log(entries);
 
-for (const [key, { open, close }] of entries) {
-  console.log(`On ${key} we open at ${open}, close at ${close}`);
-}
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open}, close at ${close}`);
+// }
 
 //
 // Optional chaning (?.)
@@ -733,6 +733,16 @@ const books = [
 // for (const book of books) {
 //   console.log(getFirstKeyword(book));
 // }
+
+// 11 - looping objects
+
+const entries = [];
+
+for (const key of Object.keys(books[0].thirdParty.goodreads)) {
+  entries.push(key);
+}
+
+console.log(entries);
 
 //
 //
