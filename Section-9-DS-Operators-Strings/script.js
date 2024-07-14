ad0c58c4426f08bfd8b379f687879113e5a38551
Edvinas Bureika <edvinasbureika@gmail.com>
2024-07-14 19:21:38 +0300

    Sets

--- Section-9-DS-Operators-Strings/script.js
+++ Section-9-DS-Operators-Strings/script.js
@@ -55,6 +55,21 @@ const restaurant = {
 };
 
 //
+// Sets
+//
+
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+
+console.log(ordersSet);
+
+//
 // Object looping
 //
 
@@ -892,70 +907,70 @@ const books = [
 // Lewandowski: 2
 // }
 
-const game = {
-  team1: "Bayern Munich",
-  team2: "Borrussia Dortmund",
-  players: [
-    [
-      "Neuer",
-      "Pavard",
-      "Martinez",
-      "Alaba",
-      "Davies",
-      "Kimmich",
-      "Goretzka",
-      "Coman",
-      "Muller",
-      "Gnarby",
-      "Lewandowski",
-    ],
-    [
-      "Burki",
-      "Schulz",
-      "Hummels",
-      "Akanji",
-      "Hakimi",
-      "Weigl",
-      "Witsel",
-      "Hazard",
-      "Brandt",
-      "Sancho",
-      "Gotze",
-    ],
-  ],
-  score: "4:0",
-  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
-  date: "Nov 9th, 2037",
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: "Bayern Munich",
+//   team2: "Borrussia Dortmund",
+//   players: [
+//     [
+//       "Neuer",
+//       "Pavard",
+//       "Martinez",
+//       "Alaba",
+//       "Davies",
+//       "Kimmich",
+//       "Goretzka",
+//       "Coman",
+//       "Muller",
+//       "Gnarby",
+//       "Lewandowski",
+//     ],
+//     [
+//       "Burki",
+//       "Schulz",
+//       "Hummels",
+//       "Akanji",
+//       "Hakimi",
+//       "Weigl",
+//       "Witsel",
+//       "Hazard",
+//       "Brandt",
+//       "Sancho",
+//       "Gotze",
+//     ],
+//   ],
+//   score: "4:0",
+//   scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+//   date: "Nov 9th, 2037",
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 
-// 1.
-for (const [index, name] of game.scored.entries()) {
-  console.log(`Goal ${index + 1}: ${name}`);
-}
+// // 1.
+// for (const [index, name] of game.scored.entries()) {
+//   console.log(`Goal ${index + 1}: ${name}`);
+// }
 
-// 2.
-let avgOdd = 0;
-for (const value of Object.values(game.odds)) {
-  avgOdd += value;
-}
-avgOdd /= Object.values(game.odds).length;
-console.log(avgOdd);
+// // 2.
+// let avgOdd = 0;
+// for (const value of Object.values(game.odds)) {
+//   avgOdd += value;
+// }
+// avgOdd /= Object.values(game.odds).length;
+// console.log(avgOdd);
 
-// 3.
-for (const [team, odd] of Object.entries(game.odds))
-  team === "x"
-    ? console.log(`Odd of draw: ${odd}`)
-    : console.log(`Odd of victory ${team}: ${odd}`);
+// // 3.
+// for (const [team, odd] of Object.entries(game.odds))
+//   team === "x"
+//     ? console.log(`Odd of draw: ${odd}`)
+//     : console.log(`Odd of victory ${team}: ${odd}`);
 
-// 4.
-let scorers = {};
-for (const scorer of game.scored) {
-  scorers[scorer] ? ++scorers[scorer] : (scorers[scorer] = 1);
-}
+// // 4.
+// let scorers = {};
+// for (const scorer of game.scored) {
+//   scorers[scorer] ? ++scorers[scorer] : (scorers[scorer] = 1);
+// }
 
-console.log(scorers);
+// console.log(scorers);
