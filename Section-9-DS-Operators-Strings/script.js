bef1ae99ac01f8a73d90dec4ef5551d7d54027e1
Edvinas Bureika <edvinasbureika@gmail.com>
2024-07-16 22:10:09 +0300

    Average time

--- Section-9-DS-Operators-Strings/script.js
+++ Section-9-DS-Operators-Strings/script.js
@@ -1169,3 +1169,6 @@ console.log(events);
 // 2. Remove unfair yellow card
 gameEvents.delete(64);
 console.log(gameEvents);
+
+// Average event time
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
