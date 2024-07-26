1b09e8998dc665e5d8132e507e7a7fe3428df65e
Edvinas Bureika <edvinasbureika@gmail.com>
2024-07-27 00:11:45 +0300

    practice part 1

--- Section-9-DS-Operators-Strings/script.js
+++ Section-9-DS-Operators-Strings/script.js
@@ -58,55 +58,55 @@ const restaurant = {
 // Strings pt 3
 //
 
-// splits string into substr by + sign, returns arr
-console.log("a+very+nice+str".split("+"));
+// // splits string into substr by + sign, returns arr
+// console.log("a+very+nice+str".split("+"));
 
-const [firstName, lastName] = "Edvinas Kazkas".split(" ");
-const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
-console.log(newName);
+// const [firstName, lastName] = "Edvinas Kazkas".split(" ");
+// const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(" ");
-  const capitalizedNames = [];
+// const capitalizeName = function (name) {
+//   const names = name.split(" ");
+//   const capitalizedNames = [];
 
-  for (const word of names)
-    capitalizedNames.push(word[0].toUpperCase() + word.slice(1));
+//   for (const word of names)
+//     capitalizedNames.push(word[0].toUpperCase() + word.slice(1));
 
-  return capitalizedNames.join(" ");
-};
+//   return capitalizedNames.join(" ");
+// };
 
-const passenger = "jessica ann smith davis";
+// const passenger = "jessica ann smith davis";
 
-console.log(capitalizeName(passenger));
+// console.log(capitalizeName(passenger));
 
-// padding
-const message = "Go to gate 23!";
-console.log(message.padStart(23, "+").padEnd(35, "+"));
+// // padding
+// const message = "Go to gate 23!";
+// console.log(message.padStart(23, "+").padEnd(35, "+"));
 
-const maskCreditCard = function (number) {
-  const str = number + "";
-  const last = str.slice(-4);
+// const maskCreditCard = function (number) {
+//   const str = number + "";
+//   const last = str.slice(-4);
 
-  return last.padStart(str.length, "*");
-};
+//   return last.padStart(str.length, "*");
+// };
 
-console.log(maskCreditCard(236423642386492364));
-console.log(maskCreditCard("0989678958956"));
+// console.log(maskCreditCard(236423642386492364));
+// console.log(maskCreditCard("0989678958956"));
 
-// repeat
-const message2 = "Bad weather... All departures delayed ";
-console.log(message2.repeat(3));
+// // repeat
+// const message2 = "Bad weather... All departures delayed ";
+// console.log(message2.repeat(3));
 
-const planesInLine = function (number) {
-  console.log(
-    `There are ${number} planes waiting in line ${"✈️".repeat(number)}`
-  );
-};
+// const planesInLine = function (number) {
+//   console.log(
+//     `There are ${number} planes waiting in line ${"✈️".repeat(number)}`
+//   );
+// };
 
-planesInLine(1);
-planesInLine(12);
-planesInLine(3);
-planesInLine(5);
+// planesInLine(1);
+// planesInLine(12);
+// planesInLine(3);
+// planesInLine(5);
 
 //
 // Strings pt 2
@@ -1409,3 +1409,21 @@ GOOD LUCK 😀
 //   else if (title.toLowerCase().includes('algorithms') && title.toLowerCase().includes('structures')) console.log('This book is about algorithms and data structures');
 //   else if (!title.toLowerCase().includes('operating') && title.endsWith('system') || title.endsWith('systems')) console.log('This book is about some systems, but definitely not about operating systems');
 // }
+
+//
+// Practice: Strings pt3
+//
+
+// 1.
+
+const bookCategories = 'science;computing;computer science;algorithms;business;operating systems;networking;electronics';
+
+const logBookCategories = function(categories) {
+  const cats = categories.split(';');
+
+  for (const cat of cats) console.log(cat);
+}
+
+logBookCategories(bookCategories);
+
+
